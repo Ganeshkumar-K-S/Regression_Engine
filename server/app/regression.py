0b30304ebbc926773d7fa9917b9340e477fb92db
Ganeshkumar-k-s <ganeshkumar78602005@gmail.com>
2025-06-18 12:46:02 +0530

    regression code updated

--- server/app/regression.py
+++ server/app/regression.py
@@ -1,1 +1,18 @@
-print("hello")
+import statsmodels.api as sm
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_absolute_error as mae, mean_squared_error as mse
+
+class Model:
+    def __init__(self,df,target,features,test_size=0.2,random_state=42):
+        self.df=df
+        self.target=target
+        self.features=features
+
+        self.X=sm.add_constant(self.df[*self.features])
+        self.y=self.df[self.target]
+
+        self.X_train,self.X_test,self.Y_train,self.Y_test=train_test_split(self.X,self.y,test_size=test_size,random_state=random_state)
+        self.__model=sm.OLS(self.y_train,self.X_train)
+
+    def getModel(self):
+        return self.__model
