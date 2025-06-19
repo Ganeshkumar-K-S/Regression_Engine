500d9dbcb2a0826ebc70e718270e6cc08fe2e803
Harivansh B <bandaruharivansh@gmail.com>
2025-06-19 18:33:15 +0530

    Initial client update

--- server/app/routes.py
+++
@@ -1,9 +0,0 @@
-import os
-from flask import Blueprint, request, jsonify, current_app
-
-engine=Blueprint('engine',__name__)
-
-@engine.route('/')
-def sendmodel(model):
-    return model.getModel()
-
