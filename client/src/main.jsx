d0f40fb9e1c685b1ac64093add8fd9bc2f524934
Harivansh B <bandaruharivansh@gmail.com>
2025-06-20 09:32:20 +0530

    New update

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,17 +1,10 @@
-// main.jsx or index.jsx
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App.jsx';
-import './index.css';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 
-const rootElement = document.getElementById('root');
-
-if (rootElement) {
-  createRoot(rootElement).render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );
-} else {
-  console.error("Root element not found.");
-}
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
